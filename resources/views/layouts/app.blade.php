7da0c0b7140b21bf5a6e4078c6af151187d4a6ea
Aadhar gaur <aadhar41@gmail.com>
2023-03-12 00:02:50 +0530

    Minor changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,12 @@
 
 <head>
     <meta charset="UTF-8">
+    @if ($title)
+    <title>{{ ucfirst($title) }}</title>
+    @else
     <title>{{ config('app.name') }}</title>
+    @endif
+    
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
 
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
--- resources/views/partials/_datatableAssests.blade.php
+++ resources/views/partials/_datatableAssests.blade.php
@@ -6,7 +6,6 @@
 <!-- Bootstrap JavaScript -->
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 
-<script src="https://cdn.datatables.net/1.13.3/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/2.3.5/js/dataTables.buttons.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
