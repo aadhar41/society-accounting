c9cff686acce5ab08ad05d3b933b30896e33b7c8
Aadhar gaur <aadhar41@gmail.com>
2023-04-02 16:20:24 +0530

    - Society Module TestCases Completed for API Routes and Web Routes - Migration Added for Countries, States and Cities - phpunit.xml updated for sqlite testing

---
+++ database/migrations/2023_04_02_064623_create_countries_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->integer('id', true)->unique('id');
+            $table->char('countryCode', 2)->default('');
+            $table->string('name', 45)->default('');
+            $table->string('status', 20)->nullable()->default('Inactive');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+};
---
+++ database/migrations/2023_04_02_064623_create_states_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('states', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('state_title', 255);
+            $table->string('state_description', 255)->nullable();
+            $table->string('status', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('states');
+    }
+};
